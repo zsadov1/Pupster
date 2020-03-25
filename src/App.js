b0250dc45cc4512c3b911f70376c7993b5a1d380
zsadov1 <zsadov1@gmail.com>
2020-03-25 19:50:29 -0400

    Pups

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import About from "./components/pages/About";
+import Discover from "./components/pages/Discover";
+import Search from "./components/pages/Search";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/discover" component={Discover} />
+        <Route path="/search" component={Search} />
+      </div>
+    </Router>
   );
 }
 
